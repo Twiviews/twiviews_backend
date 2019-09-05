203007f884b134874688f4efe5de204d2baecec4
Niva Dow <shriniva@gmail.com>
2019-09-04 22:07:39 -0400

    signal float to decimal fix

--- sentimentalize.py
+++ sentimentalize.py
@@ -3,14 +3,28 @@ from __future__ import print_function
 import base64
 import json
 import boto3
+import uuid
+from decimal import Decimal
+
+
 
 def handler(event, context):
     output = []
+    hashtag=''
+
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+    table = dynamodb.Table('reviews_tbl')
+
+
 
     for record in event['records']:
 
         dict_data = base64.b64decode(record['data']).decode('utf-8').strip()
         print(dict_data)
+
+        for tag in dict_data.split():
+            if tag.startswith("#"):
+                hashtag = tag.strip("#")
 
         comprehend = boto3.client(service_name='comprehend', region_name='us-east-1')
         sentiment_all = comprehend.detect_sentiment(Text=dict_data, LanguageCode='en')
@@ -21,11 +35,30 @@ def handler(event, context):
         signal = positive - negative
         print(signal)
 
+        id = str(uuid.uuid1())
+
+        put_response = table.put_item(
+            Item ={
+                'hashtag': hashtag,
+                'review_id': id,
+                'text': dict_data,
+                'sentiment': sentiment,
+                'signal': Decimal(signal)              
+                }
+            )
+
+        print('put response{}'.format(put_response))
+
         data_record = {
+            'id': record['recordId'],
+            'hashtag': hashtag,
             'message': dict_data,
             'sentiment': sentiment,
             'signal': signal
         }
+
+
+
         print(data_record)
 
         output_record = {
